01629430874d1cea4d57a3749b9a2637912e75a8
nicholas <nickkiim7@gmail.com>
2022-08-18 17:10:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+let saturdayFun = function saturdayFun(activity ="roller-skate"){
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun ()
+
+
+let mondayWork = function (activity =`go to the office`){
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork()
+
+function wrapAdjective(name) {
+    return function(value){
+        return `You are ${name}${value}${name}!`;
+    }
+}
+wrapAdjective("*")("a dedicated programmer")
